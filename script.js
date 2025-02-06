28f5506541de6d4f31184b4d216140dd95c871f2
eason112 <126652410+eason112@users.noreply.github.com>
2025-02-06 14:29:02 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let audioFiles = ['sounds/sound1.mp3', 'sounds/sound2.mp3', 'sounds/sound3.mp3']; // 音效文件列表
-let currentAudioIndex = 0; // 当前播放音效的索引
-let audioElement = document.getElementById('bg-audio'); // 获取音效元素
+let currentAudioIndex = 0; // 目前播放音效的索引
+let audioElement = document.getElementById('bg-audio'); // 獲取音效元素
 
 const modelViewer = document.querySelector('model-viewer');
     
@@ -8,12 +8,12 @@ var date = new Date();
 console.log(date.getMinutes());
 if(date.getHours()<9){
     modelViewer.src='assets/ball1.glb';
-    //modelViewer.setAttribute('ios-src', 'assets/ball1.usdz');
+    modelViewer.setAttribute('ios-src', 'assets/ball1.usdz');
     modelViewer.poster='image/ball1.png';
 }
 else{
     modelViewer.src='assets/ball.glb';
-    //modelViewer.setAttribute('ios-src', 'assets/ball.usdz');
+    modelViewer.setAttribute('ios-src', 'assets/ball.usdz');
     modelViewer.poster='image/ball.png';
 }
 function isMobileDeviceAR() {
@@ -24,9 +24,8 @@ function isMobileDeviceAR() {
 const music = document.getElementById('background-music');
 const arbutton = document.getElementById('ar');
 arbutton.addEventListener("click", function() {
-    // 尝试关闭当前页面
-    //changeAudio();  // 播放第一种音效
-    //setInterval(changeAudio, 60 * 1000);  // 每10分钟切换一次音效
+    //changeAudio();  // 播放第一種音效
+    //setInterval(changeAudio, 60 * 1000);  // 每10分鐘切換一次音效
     modelViewer.activateAR();
     //music.play();
 });
@@ -35,6 +34,6 @@ function changeAudio() {
     audioElement.src = audioFiles[currentAudioIndex];
     audioElement.play();  // 播放音效
 
-    // 更新当前音效索引，循环播放音效
+    // 更新目前音效索引，循環播放音效
     currentAudioIndex = (currentAudioIndex + 1) % audioFiles.length;
 }
